424d4b7a29a0048bfe68e637ad3a0c9c36b41ba4
Tarso Rosa <tarsorobertors@gmail.com>
2026-04-09 10:42:01 -0300

    Ajuste de código nos testes: timeout

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -7,31 +7,36 @@ describe('template spec', () => {
     cy.visit(baseUrl)
   })
 
-  it.only('Pesquisar termo existente', () => {
+  it('Pesquisar termo existente', () => {
     cy.get('[aria-label="Search icon link"]', {timeout: 10000})
       .should('be.visible')
-      .filter(':visible')
-      .click({ force: true })
+      .first()
+      .click()
+      //.filter(':visible')
+      //.click({ force: true })
 
-    cy.get('input[placeholder="Digite sua busca"]')
+    cy.get('input[placeholder="Digite sua busca"]',  { timeout: 10000 })
+      .should('be.visible')
       .type('imposto de renda{enter}')
 
-    cy.get('.post-item')
+    cy.get('.post-item',  { timeout: 10000 })
       .should('have.length.greaterThan', 0)
   })
 
   it('Pesquisar termo inexistente', () => {
-    cy.get('[aria-label="Search icon link"]')
+    cy.get('[aria-label="Search icon link"]', { timeout: 10000 })
       .should('be.visible')
-      .filter(':visible')
+      .first()
       .click()
+      //.filter(':visible')
+      //.click()
 
-    cy.get('input[placeholder="Digite sua busca"]')
+    cy.get('input[placeholder="Digite sua busca"]', { timeout: 10000 })
+      .should('be.visible')  
       .type('termoInexistente123{enter}')
+    
+    cy.contains('Nenhum resultado', {timeout: 10000})
+      .should('be.visible')  
 
-    cy.get('.post-item')
-      .should('have.length.greaterThan', 0)
   })
-
-
 })
